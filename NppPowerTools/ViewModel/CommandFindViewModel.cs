121f78e5e24d8838127a3c7acdb59f6993a68611
Sébastien Geiser <sgeiser@precitrame.com>
2019-05-03 16:49:38 +0200

    oops

---
+++ NppPowerTools/ViewModel/CommandFindViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Xaml;
+using Media = System.Windows.Media;
+
+namespace NppPowerTools.ViewModel
+{
+    public class CommandFindViewModel
+    {
+    }
+}
